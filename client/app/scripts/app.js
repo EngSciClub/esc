e4b544def9861625ddecbf1d3fc3ab5321bd50a4
John Liu <johnyuhanliu@gmail.com>
2013-10-04 03:38:48 -0400

    Combine models before controllers.

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -6,8 +6,8 @@ var App = window.App = Ember.Application.create();
 
 /* Order and include as you please. */
 require('scripts/plugins/*');
-require('scripts/controllers/*');
 require('scripts/models/*');
+require('scripts/controllers/*');
 require('scripts/routes/*');
 require('scripts/components/*');
 require('scripts/views/*');
