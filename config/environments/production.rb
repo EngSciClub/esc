59a0ef38576cd704f27eb9823657ec1f21cc2049
King Long Barry Fung <barry.kl.fung@gmail.com>
2015-02-21 02:20:01 -0500

    desparation moves

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ App::Application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -71,8 +71,9 @@ App::Application.configure do
     port:                 587,
     user_name: ENV["WEB_EMAIL"],
     password: ENV["WEB_PASSWORD"],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+    authentication: login
+    enable_starttls_auto: true  
+    }
   config.action_mailer.raise_delivery_errors = true
 
 
