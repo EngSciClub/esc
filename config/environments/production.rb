dafa140c344fe6341496f782b64c78e8063ec9ec
King Long Barry Fung <barry.kl.fung@gmail.com>
2015-02-21 02:30:38 -0500

    trying out sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,16 +64,15 @@ App::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    user_name: ENV["WEB_EMAIL"],
-    password: ENV["WEB_PASSWORD"],
-    authentication: login
-    enable_starttls_auto: true  
-    }
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+  }
   config.action_mailer.raise_delivery_errors = true
 
 
